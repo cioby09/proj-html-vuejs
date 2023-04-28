653cd253512852a97066b419693d0c04bba3ef7f
cioby09 <claudiociobanu09@gmail.com>
2023-04-28 17:16:34 +0200

    Section 3

--- src/components/Section3.vue
+++ src/components/Section3.vue
@@ -1,5 +1,67 @@
-<script></script>
+<script>
+export default {
+    name: "Section3",
+    data() {
+        return {
+            events: [
+                {
+                    img: 'h-2-blog-img-1.jpg',
+                    title: 'This way, loves: a detailed guide through new design',
+                    author: 'By Emily Fields'
+                },
+                {
+                    img: 'h-2-blog-img-2.jpg',
+                    title: 'I try to give people a different way of looking art',
+                    author: 'By Emily Fields'
+                },
+                {
+                    img: 'h-2-blog-img-3.jpg',
+                    title: 'Introduce Richard Laperrière of those amazing features',
+                    author: 'By Emily Fields'
+                },
+            ]
+        }
+    },
+    methods: {
+        getImagePath(img) {
+            return new URL(`../assets/img/${img}`, import.meta.url).href;
+        }
+    }
+}
+</script>
 
-<template></template>
+<template>
+    <section class="section3">
+        <div class="container">
+            <div v-for="element in events" class="event-card">
+                <img :src="getImagePath(element.img)" alt="">
+                <h3>{{ element.title }}</h3>
+                <p>{{ element.author }}</p>
+            </div>
+        </div>
+    </section>
+</template>
 
-<style></style>
+<style scoped lang="scss">
+.section3 {
+    width: 100%;
+    height: 400px;
+    background-color: white;
+
+    .container {
+        max-width: 800px;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+
+        .event-card {
+            width: calc(100% / 3);
+
+            h3 {
+                font-size: 1rem;
+            }
+        }
+    }
+}
+</style>
