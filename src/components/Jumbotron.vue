acb19cab70b45fdb5ecda13923a67491e08fa102
cioby09 <claudiociobanu09@gmail.com>
2023-04-28 17:47:55 +0200

    Commit

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -11,7 +11,18 @@ export default {
 
 <template>
     <section class="jumbotron">
-
+        <div class="container">
+            <div class="info">
+                <h1>Devotion that never ends</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium tenetur corrupti similique quaerat
+                    sapiente distinctio consequuntur porro, sint nostrum sit at eum sunt numquam in maxime aut et
+                    perferendis ad.</p>
+                <div class="btn btn-primary">Read more</div>
+            </div>
+            <div class="image">
+                <img src="../assets/img/h-2-slider-img-11.png" alt="">
+            </div>
+        </div>
     </section>
 </template>
 
@@ -20,6 +31,20 @@ export default {
     height: 350px;
     width: 100%;
     background-color: white;
-    border: 2px solid black;
+
+    .container {
+        max-width: 1000px;
+        height: 100%;
+        display: flex;
+        align-items: center;
+
+        .info {
+            width: 40%;
+        }
+
+        .image {
+            width: 60%;
+        }
+    }
 }
 </style>
