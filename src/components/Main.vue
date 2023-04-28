2e5d057c5b599fc9a4d08fdc2b287910f5fd4863
cioby09 <claudiociobanu09@gmail.com>
2023-04-28 17:06:02 +0200

    Section 2

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,13 +2,17 @@
 import Jumbotron from "./Jumbotron.vue";
 import Cards from "./Cards.vue";
 import Section1 from "./Section1.vue";
+import Section2 from "./Section2.vue";
+import Section3 from "./Section3.vue";
 
 export default {
     name: "Main",
     components: {
         Jumbotron,
         Cards,
-        Section1
+        Section1,
+        Section2,
+        Section3
     }
 }
 </script>
@@ -17,6 +21,8 @@ export default {
     <Jumbotron />
     <Cards />
     <Section1 />
+    <Section2 />
+    <Section3 />
 </template>
 
 <style></style>
---
+++ src/components/Section2.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: "Section2"
+}
+</script>
+
+<template>
+    <section class="section2">
+        <div class="container">
+            <h2 class="title">Want to work with us? Send us a message!</h2>
+            <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ut, laborum facere corporis
+                pariatur,
+                accusantium enim hic consectetur dolorum nulla animi delectus quibusdam amet debitis ab. Fugiat quibusdam
+                obcaecati ex?</p>
+            <div class="btn btn-primary">Contact</div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+.section2 {
+    width: 100%;
+    height: 300px;
+    background-color: #FAF7F1;
+}
+
+.container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>
